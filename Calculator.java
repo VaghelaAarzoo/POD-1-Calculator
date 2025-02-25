5ae6dd46f4b74a68a11e2d935d17f03dd0781ed2
Vaghela Aarzoo <vagaarzo@publicisgroupe.net>
2025-02-25 14:04:18 +0530

    add multiply function

--- Calculator.java
+++ Calculator.java
@@ -3,11 +3,16 @@ public class Calculator{
         return firstNumber+secondNumber;
     }
 
+    public static int multiply(int firstNumber, int secondNumber){
+        return firstNumber*secondNumber;
+    }
     public static void main(String[] args) {
         int firstNumber = 2;
         int secondNumber = 5;
 
         int addedNumber  = add(firstNumber, secondNumber);
         System.out.println("The added number is: " + addedNumber);
+        int multipliedNumber  = multiply(firstNumber, secondNumber);
+        System.out.println("The added number is: " + multipliedNumber);
     }
 }
